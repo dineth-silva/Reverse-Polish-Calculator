01d5adaf8107c279702cb641913ad71761656539
Navod Senarathne <88228646+navods@users.noreply.github.com>
2024-06-01 21:59:17 +0530

    Create input_buffer.c

---
+++ input_buffer.c
@@ -0,0 +1,15 @@
+#include "calculator.h"
+
+char buf[BUFSIZE];  /* buffer for ungetch */
+int bufp = 0;       /* next free position in buffer */
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
